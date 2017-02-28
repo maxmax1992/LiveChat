3df93f751bc49bf5da7225c249050ec4694a576a
Afteniy Maxim <maxim.afteniy@aalto.fi>
2017-02-28 14:56:53 +0200

    Doing heroku deployment

--- LiveChat/asgi.py
+++ LiveChat/asgi.py
@@ -1,5 +1,5 @@
 import os
 import channels.asgi
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatz.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LiveChat.settings")
 channel_layer = channels.asgi.get_channel_layer()
--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn LiveChat.Home.asgi --log-file -
-worker: python manage.py runserver
+web: daphne LiveChat.asgi:channel_layer --port $PORT --bind 0.0.0.0 -v2
+worker: python manage.py runworker -v2
