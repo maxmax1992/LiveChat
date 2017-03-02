226ad4a05bc2e3d577f37e3b4fef3804cd0b4f12
Afteniy Maxim <maxim.afteniy@aalto.fi>
2017-03-02 12:14:37 +0200

    message.fiexes

--- Home/static/Home/chat.js
+++ Home/static/Home/chat.js
@@ -13,79 +13,48 @@ $('document').ready(function () {
         var user = $("#user").val();
         var msgUser = JSON.parse(e.data);
 
-
-        //if user == msgUser create div with text to the left
-        //append div to a $('#hatWidnow') div with a chat bubble to a left
-        // console.log(user + "," + msgUser.username);
-
-        // <div class="panel panel-default" style="background-color: lightblue; text-align: right">
-        //                             </p>
-        //                             {{ message.text }} <span class="datetime">{{ message.getTime }}</span>
-        //                             </p>
-        //                         </div>
         if (user === msgUser.username) {
             var div = $('<div>').addClass("panel panel-default").css({
                 'background-color': 'lightblue',
                 'text-align': 'right',
             });
-            //             function formatDate(date) {
-            //     var hours = date.getHours();
-            //     var minutes = date.getMinutes();
-            //     var ampm = hours >= 12 ? 'pm' : 'am';
-            //     hours = hours % 12;
-            //     hours = hours ? hours : 12; // the hour '0' should be '12'
-            //     minutes = minutes < 10 ? '0' + minutes : minutes;
-            //     var strTime = hours + ':' + minutes + ' ' + ampm;
-            //     return date.getDate() + 1 + "/" + date.getMonth() + "/" + date.getFullYear() + "  " + strTime;
-            // }
-            //
-            // $(document).ready(function () {
-            //     $('.datetime').text(function () {
-            //         var date = new Date(parseInt($(this).text()));
-            //         {#                console.log(formatDate(date));#}
-            //         $(this).text(formatDate(date));
-            //         $('body').fadeIn();
-            //     });
-            // });
+
+            var d = new Date();
+
+            var month = d.getMonth() + 1;
+            var day = d.getDate();
+
+
             var text = msgUser.message;
-            var p = $('<p>').appendData([text]);
-            div.appendChild(p);
+            div.append("<p>" + text + "   " + formatDate(d) + "</p>");
+            $(".Content").append(div);
+            $('#chatWindow').scrollTop($('#chatWindow')[0].scrollHeight);
 
 
-            console.log('same user');
         } else {
-            //                 <div class="panel panel-default">
-            //                     <p>
-            //                         {{ message.text }} <span class="datetime">{{ message.getTime }}</span>
-            //                     </p>
-            //                 </div>
-            //             </div>
-            //append div to a $('#hatWidnow') div with a chat bubble to a right
 
+            var d = new Date();
+
+            var month = d.getMonth() + 1;
+            var day = d.getDate();
+
+
+            var text = msgUser.message;
+            div.append("<p>" + text + "   " + formatDate(d) + "</p>");
+            $(".Content").append(div);
+            $('#chatWindow').scrollTop($('#chatWindow')[0].scrollHeight);
             console.log("not a same user")
         }
-
-
-        // $("textarea").append("\n" + e.data);
     }
     socket.onopen = function () {
         // socket.send("hello world");
     }
-    // Call onopen directly if socket is already open
     if (socket.readyState == WebSocket.OPEN) socket.onopen();
 
 
     //sends to consumers the text with attribute 'Text' the shit.
     $("#chatform").submit(function (event) {
-        // console.log($("#chatform input").val());
-        // var titles = [];
-        // $('input[name^=titles]').each(function(){
-        //     titles.push($(this).val());
-        // });
-        // data = {
-        //     "str": $("#chatform #text").val().toString();
-        //
-        // };
+
         $('#chatWindow').scrollTop($('#chatWindow')[0].scrollHeight);
         var data = {
             'username': $("#user").val(),
@@ -97,15 +66,4 @@ $('document').ready(function () {
         event.preventDefault();
     });
 
-    // Hook up send button to send a message
-    // roomdiv.find("form").on("submit", function () {
-    //     socket.send(JSON.stringify({
-    //         "command": "send",
-    //         "room": data.join,
-    //         "message": roomdiv.find("input").val()
-    //     }));
-    //     roomdiv.find("input").val("");
-    //     return false;
-    // });
-    // $("#chats").append(roomdiv);
 });
