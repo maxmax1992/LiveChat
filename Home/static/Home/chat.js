3c913c30f14131062c90fe6fb5c41908f7f4d659
Afteniy Maxim <maxim.afteniy@aalto.fi>
2017-03-02 20:35:08 +0200

    fix

--- Home/static/Home/chat.js
+++ Home/static/Home/chat.js
@@ -10,51 +10,79 @@ $('document').ready(function () {
     console.log("ws://" + window.location.host + window.location.pathname);
     socket.onmessage = function (e) {
 
+
         var user = $("#user").val();
         var msgUser = JSON.parse(e.data);
 
-        if (user === msgUser.username) {
-            var div = $('<div>').addClass("panel panel-default").css({
-                'background-color': 'lightblue',
-                'text-align': 'right',
-            });
+        if(msgUser.message !== '') {
+            if (user === msgUser.username) {
+                var $div = $(".templateMSG").clone().removeClass("templateMSG");
+                var text = msgUser.message;
+                var d = new Date();
 
-            var d = new Date();
+                var month = d.getMonth() + 1;
+                var day = d.getDate();
+                $div.append("<span style='color: black; font-size: 20px'>" + user + " </span>" + "<span>" + " " + formatDate(d) + "</span>" + "<br><br>" + text);
 
-            var month = d.getMonth() + 1;
-            var day = d.getDate();
-
-
-            var text = msgUser.message;
-            div.append("<p>" + text + "   " + formatDate(d) + "</p>");
-            $(".Content").append(div);
-            $('#chatWindow').scrollTop($('#chatWindow')[0].scrollHeight);
+                $(".Content").append($div);
+                $div.fadeIn(0);
+                $('#chatWindow').scrollTop($('#chatWindow')[0].scrollHeight);
 
 
-        } else {
-
-            var d = new Date();
-
-            var month = d.getMonth() + 1;
-            var day = d.getDate();
+            } else {
 
 
-            var text = msgUser.message;
-            div.append("<p>" + text + "   " + formatDate(d) + "</p>");
-            $(".Content").append(div);
-            $('#chatWindow').scrollTop($('#chatWindow')[0].scrollHeight);
-            console.log("not a same user")
+                var $div = $(".templateMSG2").clone().removeClass("templateMSG2");
+                var text = msgUser.message;
+                var d = new Date();
+
+                var month = d.getMonth() + 1;
+                var day = d.getDate();
+                $div.append("<span style='color: black; font-size: 20px'>" + user + " </span>" + "<span>" + " " + formatDate(d) + "</span>" + "<br><br>" + text);
+                // var div = $('<div>').addClass("panel panel-default").css({
+                //     'background-color': 'lightblue',
+                //     'text-align': 'right',
+                // });
+                //
+
+                //
+                //
+                // var text = msgUser.message;
+                // div.append("<p>" + text + "   " + formatDate(d) + "</p>");
+                $(".Content").append($div);
+                $div.fadeIn(0);
+                $('#chatWindow').scrollTop($('#chatWindow')[0].scrollHeight);
+
+                //
+                // var d = new Date();
+                //
+                // var month = d.getMonth() + 1;
+                // var day = d.getDate();
+                //
+                //
+                // var text = msgUser.message;
+                // div.append("<p>" + text + "   " + formatDate(d) + "</p>");
+                // $(".Content").append(div);
+                // $('#chatWindow').scrollTop($('#chatWindow')[0].scrollHeight);
+                // console.log("not a same user")
+            }
+            $('#msg').val('');
         }
+
     }
     socket.onopen = function () {
-        // socket.send("hello world");
+        $('#chatWindow').scrollTop($('#chatWindow')[0].scrollHeight);
     }
     if (socket.readyState == WebSocket.OPEN) socket.onopen();
 
+    $("#chatform").submit(function(event) {
+        sendEvent(event);
+    });
+    $("#go").click(function(event) {
+        sendEvent(event);
+    });
 
-    //sends to consumers the text with attribute 'Text' the shit.
-    $("#chatform").submit(function (event) {
-
+    function sendEvent(event) {
         $('#chatWindow').scrollTop($('#chatWindow')[0].scrollHeight);
         var data = {
             'username': $("#user").val(),
@@ -64,6 +92,6 @@ $('document').ready(function () {
         socket.send(JSON.stringify(data));
 
         event.preventDefault();
-    });
+    }
 
 });
